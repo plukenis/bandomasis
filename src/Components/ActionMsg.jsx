ccbe3c12e49b53e6d72ad5631a7ebb3920236d3e
plukenis <ieva.zaboole@gmail.com>
2021-11-24 15:11:13 +0200

    mesg styled

--- src/Components/ActionMsg.jsx
+++ src/Components/ActionMsg.jsx
@@ -1,9 +1,8 @@
 function ActionMsg({msg, showMsg}) {
 
     return (
-        <div className="action-msg" style={{
+        <div className="mesg" style={{
             height: showMsg ? '60px' : '0',
-            // border: showMsg ? '1px solid #fadc64' : '1px solid transparent',
             top: showMsg ? window.scrollY : -2
             }}>
             <span>{msg}</span>
